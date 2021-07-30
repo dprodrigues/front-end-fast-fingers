93bb475fa85d2b77daa00bed7e61a2796937130a
dprodrigues <dandan.dpr@gmail.com>
2021-07-30 20:54:18 -0300

    feat: add general context

---
+++ src/context/General.js
@@ -0,0 +1,49 @@
+import { createContext, useContext, useReducer } from "react";
+
+const initialState = {
+    loggedIn: false,
+    selectedLanguage: undefined,
+};
+
+const reducer = (state, action) => {
+    switch (action.type) {
+        case "SET_LOGGED_IN_VALUE": {
+            return { ...state, loggedIn: action.payload.loggedIn };
+        }
+
+        case "SET_SELECTED_LANGUAGE": {
+            return {
+                ...state,
+                selectedLanguage: action.payload.selectedLanguage,
+            };
+        }
+
+        default: {
+            return state;
+        }
+    }
+};
+
+const Context = createContext(undefined);
+
+const GeneralProvider = ({ children }) => {
+    const [state, dispatch] = useReducer(reducer, initialState);
+
+    return (
+        <Context.Provider value={{ state, dispatch }}>
+            {children}
+        </Context.Provider>
+    );
+};
+
+const useGeneral = () => {
+    const context = useContext(Context);
+
+    if (!context) {
+        throw new Error("useGeneral must be used within a useGeneral");
+    }
+
+    return context;
+};
+
+export { GeneralProvider, useGeneral };
